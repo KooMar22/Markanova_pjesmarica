0125c0500ccc1c51161fd45c0d9d80a7df035876
KooMar <djhracak@gmail.com>
2024-04-26 19:35:36 +0200

    Some UI adjustments

--- markan_player.py
+++ markan_player.py
@@ -1,5 +1,5 @@
 # Import modules
-import os, time
+import time
 from random import sample
 from tkinter import *
 from tkinter import filedialog, ttk
@@ -11,6 +11,7 @@ from mutagen.mp3 import MP3
 class MediaPlayer:
     def __init__(self):
         self.root = Tk()
+        self.root.config(bg="light yellow")
         mixer.init()
         
         self.root.title("Markanova Pjesmarica")
@@ -18,15 +19,20 @@ class MediaPlayer:
         self.root.iconbitmap("imgs/music_notes_icon.ico")
 
         # Add Frames - Playlist Frame, Music Info Frame and Control Panel Frame
-        self.playlist_frame = Frame(self.root)
+        self.playlist_frame = Frame(self.root, bg="light yellow")
         self.playlist_frame.grid(column=0, row=0, padx=10, pady=(0, 10))
 
-        self.music_info_frame = Frame(self.root)
+        self.music_info_frame = Frame(self.root, bg="light yellow")
         self.music_info_frame.grid(column=0, row=1)
 
-        self.control_panel = Frame(self.root)
+        self.control_panel = Frame(self.root, bg="light yellow")
         self.control_panel.grid(column=0, row=2, padx=10, pady=10)
         self.control_panel.configure(border=1, relief="flat", borderwidth=2)
+
+        # Add some style
+        style = ttk.Style()
+        style.configure("TScale", background="white")
+
 
         # Music Menu
         self.menu = Menu(self.root)
@@ -40,26 +46,29 @@ class MediaPlayer:
 
         # Playlist listbox
         self.playlist_listbox = Listbox(self.playlist_frame, width=110, height=20,
-                                selectbackground="gray", selectforeground="black",
-                                selectmode=SINGLE)
+                                selectbackground="gold", selectforeground="black",
+                                selectmode=SINGLE, bg="light yellow")
         self.playlist_listbox.pack(fill=BOTH, expand=True)
         self.playlist_listbox.bind("<<ListboxSelect>>", self.play_song)
 
         # Add a Status Info about currently playing song
         self.status_variable = StringVar()
         self.status_variable.set("")
-        self.status_lbl = Label(self.music_info_frame, textvariable=self.status_variable)
+        self.status_lbl = Label(self.music_info_frame, bg="light yellow",
+                                font=("Arial", 12, "bold", "italic"),
+                                textvariable=self.status_variable)
         self.status_lbl.grid(column=0, row=0, padx=10, pady=10, sticky="ew")
 
         # Add Progress Slider
-        self.progress_slider = ttk.Scale(self.music_info_frame, length=600,
+        self.progress_slider = ttk.Scale(self.music_info_frame, length=620,
                                          from_=0, to=100, orient="horizontal",
-                                         value=0, command=self.slide)
+                                         value=0, command=self.slide,
+                                         style="TScale")
         self.progress_slider.grid(column=0, row=1, padx=10, sticky="w")
 
 
         # Add a song time Label
-        self.song_time_lbl = Label(self.music_info_frame, text="")
+        self.song_time_lbl = Label(self.music_info_frame, text="", bg="light yellow")
         self.song_time_lbl.grid(column=1, row=1, padx=5)
 
         # Initialize song_paths lists
@@ -88,27 +97,31 @@ class MediaPlayer:
 
         # Play and Pause button
         self.play_pause_btn = Button(self.control_panel, image=self.play_img,
-                                     command=self.play_or_pause)
+                                     command=self.play_or_pause, bg="light yellow")
         self.play_pause_btn.image = self.play_img
         self.play_pause_btn.grid(column=2, row=0, padx=10, pady=10)
 
         # Stop button
-        self.stop_btn = Button(self.control_panel, image=self.stop_img, command=self.stop_song)
+        self.stop_btn = Button(self.control_panel, image=self.stop_img,
+                               command=self.stop_song, bg="light yellow")
         self.stop_btn.image = self.stop_img
         self.stop_btn.grid(column=3, row=0, padx=10, pady=10)
 
         # Shuffle button
-        self.shuffle_btn = Button(self.control_panel, image=self.shuffle_on_img, command=self.shuffle)
+        self.shuffle_btn = Button(self.control_panel, image=self.shuffle_on_img,
+                                  command=self.shuffle, bg="light yellow")
         self.shuffle_btn.image = self.shuffle_btn
         self.shuffle_btn.grid(column=0, row=0, padx=10, pady=10)
 
         # Backward button
-        self.back_btn = Button(self.control_panel, image=self.back_img, command=self.backward)
+        self.back_btn = Button(self.control_panel, image=self.back_img,
+                               command=self.backward, bg="light yellow")
         self.back_img.image = self.back_img
         self.back_btn.grid(column=1, row=0, padx=10, pady=10)
 
         # Forward
-        self.fwd_btn = Button(self.control_panel, image=self.fwd_img, command=self.forward)
+        self.fwd_btn = Button(self.control_panel, image=self.fwd_img,
+                              command=self.forward, bg="light yellow")
         self.fwd_btn.image= self.fwd_img
         self.fwd_btn.grid(column=4, row=0, padx=10, pady=10)
 
@@ -116,7 +129,8 @@ class MediaPlayer:
         self.volume_variable = DoubleVar()
         self.volume_variable.set(5)
         self.volume_scale = Scale(self.control_panel, orient="horizontal", from_=0, to=10,
-                                  variable=self.volume_variable, command=self.set_volume)
+                                  variable=self.volume_variable, command=self.set_volume,
+                                  bg="light yellow")
         self.volume_scale.grid(column=5, row=0, padx=10, pady=10)
 
         # Size of the window
@@ -140,12 +154,16 @@ class MediaPlayer:
         return ImageTk.PhotoImage(img)
 
     def extract_song_info(self, file_path):
-        """Extracts song metadata (artist and title)"""
+        """Extracts song metadata (artist, title and song length)"""
         try:
             audio = MP3(file_path)
             artist = audio['TPE1'].text[0] if "TPE1" in audio else "Unknown Artist"
             title = audio['TIT2'].text[0] if "TIT2" in audio else "Unknown Title"
-            return {"artist": artist, "title": title}
+            self.song_len = audio.info.length
+            # Convert to time format
+            mins, secs = divmod(int(self.song_len), 60)
+            self.total_time = "{:02d}:{:02d}".format(mins, secs)
+            return {"artist": artist, "title": title, "total_time": self.total_time}
         except Exception as e:
             print(f"Error extracting metadata for {file_path}: {e}")
             return None
@@ -159,7 +177,7 @@ class MediaPlayer:
             song_info = self.extract_song_info(file_path)
             if song_info:
                 # Add song info to playlist listbox
-                self.playlist_listbox.insert(END, f"{song_info['artist']} - {song_info['title']}")
+                self.playlist_listbox.insert(END, f"{song_info['artist']} - {song_info['title']} - {song_info["total_time"]}")
                 # Add file path to song_paths list
                 index = self.playlist_listbox.size() - 1
                 self.song_paths.append(file_path)
@@ -398,13 +416,6 @@ class MediaPlayer:
             # Grab the song title
             # Use song path from song_paths list
             song = self.song_paths[music_playlist[0]]
-            # Load song with Mutagen
-            song_mut = MP3(song)
-            # Get song length
-            self.song_len = song_mut.info.length
-            # Convert to time format
-            mins, secs = divmod(int(self.song_len), 60)
-            total_time = "{:02d}:{:02d}".format(mins, secs)
 
             # Increase current time by 1 sec
             current_time += 1
@@ -431,7 +442,7 @@ class MediaPlayer:
                 elapsed_time = "{:02d}:{:02d}".format(mins, secs)
 
                 # Update the song time
-                self.song_time_lbl.config(text=f"{elapsed_time} / {total_time}")
+                self.song_time_lbl.config(text=f"{elapsed_time}")
 
                 # Move this thing along by 1 sec
                 next_time = int(self.progress_slider.get()) + 1
